cbef79ae9cf3937b9b58f843d7313c34a65e4e17
JayapradaBiswal <jayaprada.nalanda@gmail.com>
2021-01-06 21:31:08 +0530

    committed

---
+++ Lifecycle_LoadOnStartup_Annotation/src/FirstServlet.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+@WebServlet(urlPatterns="/fs",loadOnStartup=3)
+public class FirstServlet extends GenericServlet
+{
+	public FirstServlet()
+	{
+		System.out.println("Servlet Object is Created");
+	}
+	@Override
+	public void init(ServletConfig config) throws ServletException
+	{
+		System.out.println("Servlet obj is Initialized!!!");
+	}
+	@Override
+	public void service(ServletRequest req, ServletResponse resp)
+			throws ServletException, IOException 
+	{
+		String name=req.getParameter("nm");
+		String place=req.getParameter("pl");
+		PrintWriter out=resp.getWriter();//SERVLET RESPONSE
+		out.println("<html><body bgcolor='yellow'>"
+		                 + "<h1>Student details are "+name+" "+place+"</h1> "
+		                 + "</body></html>");//Presentation Logic
+		out.flush();//PrintWriter
+		out.close();
+		System.out.println("service() is Executed!!");
+	}
+	@Override
+	public void destroy()
+	{
+      System.out.println("closed all costly Resources!!" );
+    }
+
+}
+
